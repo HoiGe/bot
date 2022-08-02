0c9c786f457c67165d0678456d379855fe3c5d9a
HoiGe <rd_idk5682@outlook.com>
2022-08-02 21:46:35 +0800

    Add README.md[小]

---
+++ README.md
@@ -0,0 +1,11 @@
+# ioHGe-Bot
+## 基本参数
+本机器人基于`Mirai-Kotlin`开发
+### 库
+- [Rcon](https://github.com/jobfeikens/rcon) `nl.vv32.rcon:rcon:1.2.0`
+- [Fuel](https://github.com/kittinunf/Fuel) `com.github.kittinunf.fuel:fuel:2.3.1`
+- [Fuel-Coroutines](https://github.com/kittinunf/Fuel) `com.github.kittinunf.fuel:fuel-coroutines:2.3.1`
+- [Klaxon](https://github.com/cbeust/klaxon) `com.beust:klaxon:5.5`
+### 功能主治
+- 血压低
+- 代码洁癖
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     val kotlinVersion = "1.7.10"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
-
     id("net.mamoe.mirai-console") version "2.12.0"
     kotlin("kapt") version "1.7.10"
 }
@@ -20,7 +19,6 @@ dependencies {
     implementation("nl.vv32.rcon:rcon:1.2.0")
     implementation ("com.github.kittinunf.fuel:fuel:2.3.1")
     implementation ("com.github.kittinunf.fuel:fuel-coroutines:2.3.1")
-
     implementation ("com.beust:klaxon:5.5")
 }
 
